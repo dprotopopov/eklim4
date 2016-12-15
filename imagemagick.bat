0c1efedb3e206e1112f64e5fb6521415a2bbef0b
dprotopopov <protopopov@narod.ru>
2016-12-15 21:46:15 +0300

    first commit

--- imagemagick.bat
+++ imagemagick.bat
@@ -1,2 +1,16 @@
 set IMAGEMAGICK="Y:\ImageMagick-7.0.3-Q8"
-for /R input %%f in (*.*) do %IMAGEMAGICK%\magick "input\%%~nf%%~xf" -resize "1024x1024^" -gravity center -crop "1024x1024+0+0" +repage "output\%%~nf%%~xf"
+for /R input %%f in (*.*) do call :Foo %%~nf%%~xf
+goto End
+
+:Foo
+ %IMAGEMAGICK%\magick "input\%1" -ping -format %%w info: > temp
+ set /p WIDTH= < temp
+ %IMAGEMAGICK%\magick "input\%1" -ping -format %%h info: > temp
+ set /p HEIGHT=  < temp
+ del temp
+ if %WIDTH% GEQ %HEIGHT% (set SIZE=%HEIGHT%)
+ if %WIDTH% LEQ %HEIGHT% (set SIZE=%WIDTH%)
+ %IMAGEMAGICK%\magick "input\%1" -gravity center -crop "%SIZE%x%SIZE%+0+0" +repage "output\%1"
+goto :eof
+
+:End
